27545f718c723eca63a6ef1f5deebffc4fd3d31c
RicardoAccorsi <ric.accorsi@gmail.com>
2023-07-30 18:11:36 -0300

    ajustes banco de dados

--- fakepinterest/__init__.py
+++ fakepinterest/__init__.py
@@ -6,10 +6,14 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
+import os
 
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///comunidade.db"
+# versão local
+#app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///comunidade.db"
+# após o deploy
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config["SECRET_KEY"] = "e752b3fb63c15d68ff0ea6f9051a9c7c"
 app.config["UPLOAD_FOLDER"] = "static/fotos_posts" # armazenar fotos dos usuarios nessa pasta específica
 
